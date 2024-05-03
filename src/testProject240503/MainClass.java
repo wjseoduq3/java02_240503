0e68d70698f3946206b9ba1576941ab2749d055a
wjseoduq3 <dragon65@nate.com>
2024-05-03 09:42:11 +0900

    2번째 GitHub upload 연습
    Change-Id: I1a4ed52515387d2f4365533c2fcea8fdd81a1c71

---
+++ src/testProject240503/MainClass.java
@@ -0,0 +1,10 @@
+package testProject240503;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
